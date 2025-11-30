8c667285eb31907f11bef6e11ddcc1752950ec48
Thiago Recetto Moraes <thiago.social@outlook.com>
2025-11-29 21:11:00 -0300

    Add project files.

---
+++ SistemaHospitalar_API/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+//=======================================================
+// Controllers
+//=======================================================
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//=======================================================
+// Enviroment
+//=======================================================
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
